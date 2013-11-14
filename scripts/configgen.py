4260733408fe10e3185bf36b4f174629b4cb8633
MatthiasLittmann <matthias.littmann@uni-duesseldorf.de>
2013-11-14 14:15:47 +0100

    change path and expand search pattern

--- scripts/configgen.py
+++ scripts/configgen.py
@@ -3,21 +3,48 @@ import os
 import re
 
 
+class config:
+    def __init__(self, name="", context="", typ=""):
+        self.name = name
+        self.context = context
+        self.typ = typ
+
+    def get_package(self):
+        return re.match(r""".*?([^ ]*config).*""", self.context).group(1)
+
+
+class configs:
+    def __init__(self):
+        self.confs = dict()
+
+    def update(self, conf):
+        if conf.name not in self.confs:
+            self.confs[conf.name] = list()
+        self.confs[conf.name].append(conf)
+
+cfgs = configs()
+
+
 def find_py_files(dir):
     for dirpath, dirname, filenames in os.walk(dir):
         for filename in filenames:
-            if os.path.splitext(filename)[1] == ".py":
+            if os.path.splitext(filename)[1] == ".py" or \
+                    os.path.splitext(filename)[1] == ".html":
                 yield os.path.join(dirpath, filename)
 
 
 def find_config(filename):
     with io.open(filename, 'r', encoding="UTF-8") as f:
         code = f.read()
-    for m in re.finditer(r"""(?x)config (?:\.get\(|\[)
-            '(.*?)' #matching group
-            (?:,.*?)? (?:\)|\])
-            """, code):
-        yield m.group(1)
+    for m in re.finditer(r"""(?x)^.*(config (?:\.get_?(.*?)\(|\[)
+            '(.*?)' #matching group for name
+            (?:,.*?)? (?:\)|\])).*$ # group 1 for comlete config
+            """, code, re.MULTILINE):
+        fds = config(m.group(3), m.group(), m.group(2))
+        print(m.group(2)),
+        print(m.group(1))
+        cfgs.update(fds)
+        yield m.group(3)
 
 
 def get_sorted_configs(dir):
@@ -27,5 +54,16 @@ def get_sorted_configs(dir):
     return sorted(s)
 
 
-for cfg in get_sorted_configs("../src"):
-    print(cfg)
+get_sorted_configs("../src")
+for key in cfgs.confs.viewkeys():
+    #print(key),
+    #print(": "),
+    for conf in cfgs.confs[key]:
+        #print(conf.typ),
+        None
+    #print("")
+
+ROOT = os.path.dirname(os.path.normpath(__file__))
+configs = get_sorted_configs(os.path.join(ROOT, 'src'))
+print('\n'.join(configs))
+
